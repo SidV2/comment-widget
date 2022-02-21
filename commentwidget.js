74c00e498aea81c5326f4613c823629a83670856
Sidhartha Verma <sidharthaverma@Sidharthas-MacBook-Air.local>
2022-02-21 13:39:34 -0500

    functional working

--- commentwidget.js
+++ commentwidget.js
@@ -1,6 +1,5 @@
 let commentList = [];
 let randomId = '0';
-let level = 0;
 
 function postComment() {
     let commentValue = document.getElementById('comment').value;
@@ -17,18 +16,18 @@ function createComment(commentValue, id) {
                 children: [],
             }
         )
-        showcommentOnDom(parent.children);
+        appendInExistingComment(parent, parent.children);
     } else {
+        randomId += 1;
         commentList.push(
             {
-                id: level + randomId,
+                id: randomId,
                 commentText: commentValue,
                 children: []
             }
         )
         showcommentOnDom(commentList);
     }
-    randomId += 1;
     console.log(commentList);
 }
 
@@ -67,6 +66,8 @@ function replytoComment(e) {
     postToCommentButton.innerText = 'Post Comment';
     commentDiv.appendChild(postToCommentButton);
     postToCommentButton.addEventListener('click', (e) => {
+        document.getElementById("replyCommentTextArea" + id).style.display = 'none';
+        postToCommentButton.style.display = 'none';
         createComment(document.getElementById("replyCommentTextArea" + id).value, id);
     });
 }
@@ -75,7 +76,7 @@ function findComment(id, commentList) {
     console.log(commentList);
     for (let comment of commentList) {
         if (comment.children.length) {
-            const result = this.findComment(id, comment.replies);
+            const result = this.findComment(id, comment.children);
             if (result !== -1) {
                 return result;
             }
@@ -87,7 +88,26 @@ function findComment(id, commentList) {
     return -1;
 }
 
-
+function appendInExistingComment(parent, commentList) {
+    console.log(parent);
+    let comment = commentList[commentList.length - 1];
+    let commentDiv = document.createElement("div");
+    commentDiv.setAttribute("class", "commentDiv");
+    commentDiv.setAttribute("id", "commentDiv" + comment.id);
+    commentDiv.style.border = '1px solid black';
+    commentDiv.style.marginLeft = '5px';
+    commentDiv.innerHTML = comment.commentText;
+    document.getElementById("commentDiv" + parent.id).appendChild(commentDiv);
+    let replyDiv = document.createElement("div");
+    replyDiv.setAttribute("class", "replyDiv" + comment.id);
+    replyDiv.setAttribute("id", "replyDiv" + comment.id);
+    commentDiv.appendChild(replyDiv);
+    let replyButton = document.createElement("button");
+    replyButton.setAttribute("class", "replyButton" + comment.id);
+    replyButton.innerText = "Reply to this comment";
+    replyButton.addEventListener('click', replytoComment);
+    replyDiv.appendChild(replyButton);
+}
 
 
 
